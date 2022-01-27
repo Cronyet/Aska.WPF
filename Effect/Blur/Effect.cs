d552691d3a65ba4f5b88870cec3776a93d40cc50
常青园晚 <50760269+Dynesshely@users.noreply.github.com>
2022-01-27 23:06:48 +0800

    uploaded all source files at once

---
+++ Effect/Blur/Effect.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CS8603 // 可能返回 null 引用。
+#pragma warning disable CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
+#pragma warning disable IDE0044 // 添加只读修饰符
+#pragma warning disable CA2208 // 正确实例化参数异常
+
+namespace Aska.WPF.Effect.Blur
+{
+    public static class Effect
+    {
+        private static Guid BlurEffectGuid = new("{633C80A4-1843-482B-9EF2-BE2834C5FDD4}");
+        private static Guid UsmSharpenEffectGuid = new("{63CBF3EE-C526-402C-8F71-62C540BF5142}");
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct BlurParameters
+        {
+            internal float Radius;
+            internal bool ExpandEdges;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SharpenParams
+        {
+            internal float Radius;
+            internal float Amount;
+        }
+
+        internal enum PaletteType
+        {
+            PaletteTypeCustom = 0,
+            PaletteTypeOptimal = 1,
+            PaletteTypeFixedBW = 2,
+            PaletteTypeFixedHalftone8 = 3,
+            PaletteTypeFixedHalftone27 = 4,
+            PaletteTypeFixedHalftone64 = 5,
+            PaletteTypeFixedHalftone125 = 6,
+            PaletteTypeFixedHalftone216 = 7,
+            PaletteTypeFixedHalftone252 = 8,
+            PaletteTypeFixedHalftone256 = 9
+        };
+
+        internal enum DitherType
+        {
+            DitherTypeNone = 0,
+            DitherTypeSolid = 1,
+            DitherTypeOrdered4x4 = 2,
+            DitherTypeOrdered8x8 = 3,
+            DitherTypeOrdered16x16 = 4,
+            DitherTypeOrdered91x91 = 5,
+            DitherTypeSpiral4x4 = 6,
+            DitherTypeSpiral8x8 = 7,
+            DitherTypeDualSpiral4x4 = 8,
+            DitherTypeDualSpiral8x8 = 9,
+            DitherTypeErrorDiffusion = 10
+        }
+
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipCreateEffect(Guid guid, out IntPtr effect);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipDeleteEffect(IntPtr effect);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipGetEffectParameterSize(IntPtr effect, out uint size);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipSetEffectParameters(IntPtr effect, IntPtr parameters, uint size);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipGetEffectParameters(IntPtr effect, ref uint size, IntPtr parameters);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapApplyEffect(IntPtr bitmap, IntPtr effect, ref Rectangle rectOfInterest, bool useAuxData, IntPtr auxData, int auxDataSize);
+
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapCreateApplyEffect(ref IntPtr SrcBitmap, int numInputs, IntPtr effect, ref Rectangle rectOfInterest, ref Rectangle outputRect, out IntPtr outputBitmap, bool useAuxData, IntPtr auxData, int auxDataSize);
+
+
+        // 这个函数我在C#下已经调用成功
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipInitializePalette(IntPtr palette, int palettetype, int optimalColors, int useTransparentColor, int bitmap);
+
+        // 该函数一致不成功，不过我在VB6下调用很简单，也很成功，主要是结构体的问题。
+        [DllImport("gdiplus.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int GdipBitmapConvertFormat(IntPtr bitmap, int pixelFormat, int dithertype, int palettetype, IntPtr palette, float alphaThresholdPercent);
+
+        /// <summary>
+        /// 获取对象的私有字段的值，感谢Aaron Lee Murgatroyd
+        /// </summary>
+        /// <typeparam name="TResult">字段的类型</typeparam>
+        /// <param name="obj">要从其中获取字段值的对象</param>
+        /// <param name="fieldName">字段的名称.</param>
+        /// <returns>字段的值</returns>
+        /// <exception cref="System.InvalidOperationException">无法找到该字段.</exception>
+        internal static TResult GetPrivateField<TResult>(this object obj, string fieldName)
+        {
+            if (obj == null) return default;
+            Type ltType = obj.GetType();
+            FieldInfo lfiFieldInfo = ltType.GetField(fieldName, System.Reflection.BindingFlags.GetField | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            if (lfiFieldInfo != null) return (TResult)lfiFieldInfo.GetValue(obj);
+            else throw new InvalidOperationException(string.Format("Instance field '{0}' could not be located in object of type '{1}'.", fieldName, obj.GetType().FullName));
+        }
+
+        public static IntPtr NativeHandle(this Bitmap Bmp) => Bmp.GetPrivateField<IntPtr>("nativeImage");
+
+        /// <summary>
+        /// 对图像进行高斯模糊,参考：http://msdn.microsoft.com/en-us/library/ms534057(v=vs.85).aspx
+        /// </summary>
+        /// <param name="Rect">需要模糊的区域，会对该值进行边界的修正并返回.</param>
+        /// <param name="Radius">指定高斯卷积核的半径，有效范围[0，255],半径越大，图像变得越模糊.</param>
+        /// <param name="ExpandEdge">指定是否对边界进行扩展，设置为True，在边缘处可获得较为柔和的效果. </param>
+        public static void GaussianBlur(this Bitmap Bmp, ref Rectangle Rect, float Radius = 10, bool ExpandEdge = false)
+        {
+            int Result;
+            BlurParameters BlurPara;
+            if ((Radius < 0) || (Radius > 255))
+            {
+                throw new ArgumentOutOfRangeException("半径必须在[0,255]范围内");
+            }
+            BlurPara.Radius = Radius;
+            BlurPara.ExpandEdges = ExpandEdge;
+            Result = GdipCreateEffect(BlurEffectGuid, out IntPtr BlurEffect);
+            if (Result == 0)
+            {
+                IntPtr Handle = Marshal.AllocHGlobal(Marshal.SizeOf(BlurPara));
+                Marshal.StructureToPtr(BlurPara, Handle, true);
+                _ = GdipSetEffectParameters(BlurEffect, Handle, (uint)Marshal.SizeOf(BlurPara));
+                _ = GdipBitmapApplyEffect(Bmp.NativeHandle(), BlurEffect, ref Rect, false, IntPtr.Zero, 0);
+                // 使用GdipBitmapCreateApplyEffect函数可以不改变原始的图像，而把模糊的结果写入到一个新的图像中
+                _ = GdipDeleteEffect(BlurEffect);
+                Marshal.FreeHGlobal(Handle);
+            }
+            else
+            {
+                throw new ExternalException("不支持的GDI+版本，必须为GDI+1.1及以上版本，且操作系统要求为Win Vista及之后版本.");
+            }
+        }
+
+        /// <summary>
+        /// 对图像进行锐化,参考：http://msdn.microsoft.com/en-us/library/ms534073(v=vs.85).aspx
+        /// </summary>
+        /// <param name="Rect">需要锐化的区域，会对该值进行边界的修正并返回.</param>
+        /// <param name="Radius">指定高斯卷积核的半径，有效范围[0，255],因为这个锐化算法是以高斯模糊为基础的，所以他的速度肯定比高斯模糊妈妈</param>
+        /// <param name="ExpandEdge">指定锐化的程度，0表示不锐化。有效范围[0,255]. </param>
+        public static void UsmSharpen(this Bitmap Bmp, ref Rectangle Rect, float Radius = 10, float Amount = 50f)
+        {
+            int Result;
+            SharpenParams sharpenParams;
+            if ((Radius < 0) || (Radius > 255))
+            {
+                throw new ArgumentOutOfRangeException("参数Radius必须在[0,255]范围内");
+            }
+            if ((Amount < 0) || (Amount > 100))
+            {
+                throw new ArgumentOutOfRangeException("参数Amount必须在[0,255]范围内");
+            }
+            sharpenParams.Radius = Radius;
+            sharpenParams.Amount = Amount;
+            Result = GdipCreateEffect(UsmSharpenEffectGuid, out IntPtr UnSharpMaskEffect);
+            if (Result == 0)
+            {
+                IntPtr Handle = Marshal.AllocHGlobal(Marshal.SizeOf(sharpenParams));
+                Marshal.StructureToPtr(sharpenParams, Handle, true);
+                _ = GdipSetEffectParameters(UnSharpMaskEffect, Handle, (uint)Marshal.SizeOf(sharpenParams));
+                _ = GdipBitmapApplyEffect(Bmp.NativeHandle(), UnSharpMaskEffect, ref Rect, false, IntPtr.Zero, 0);
+                _ = GdipDeleteEffect(UnSharpMaskEffect);
+                Marshal.FreeHGlobal(Handle);
+            }
+            else
+            {
+                throw new ExternalException("不支持的GDI+版本，必须为GDI+1.1及以上版本，且操作系统要求为Win Vista及之后版本.");
+            }
+        }
+    }
+}
+
+#pragma warning restore CA2208 // 正确实例化参数异常
+#pragma warning restore IDE0044 // 添加只读修饰符
+#pragma warning restore CS8600 // 将 null 字面量或可能为 null 的值转换为非 null 类型。
+#pragma warning restore CS8603 // 可能返回 null 引用。
